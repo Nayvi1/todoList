31a634e016bba454537b6b5afeaa56f6ba85c003
Hadi <frisk17h@gmail.com>
2024-03-26 22:21:12 +0430

    small css fix

--- src/features/todos/TodoPage.jsx
+++ src/features/todos/TodoPage.jsx
@@ -3,6 +3,7 @@ import Button from "../../ui/Button";
 import IsMobile from "../../utils/components/IsMobile";
 import { useSelector } from "react-redux";
 import { getGroupById } from "../group/groupSlice";
+import Todo from "./Todo";
 
 function TodoPage() {
   const { todo: todoId } = useParams();
@@ -11,7 +12,7 @@ function TodoPage() {
   return (
     <main className="max-w-[1360px] mx-auto sm:p-10 p-7">
       <div className="flex justify-between items-center">
-        <h1 className="font-bold text-3xl text-lynch-500">{group.name}</h1>
+        <h1 className="font-bold text-3xl text-lynch-900">{group.name}</h1>
         <div className="flex gap-3">
           <img src="/svg/edit.svg" className="w-10 cursor-pointer" alt="" />
           <IsMobile
@@ -26,7 +27,12 @@ function TodoPage() {
           />
         </div>
       </div>
-      <div className="flex sm:flex-wrap gap-5 mt-5 flex-col sm:flex-row"></div>
+      <div className="flex sm:flex-wrap gap-5 mt-5 flex-col sm:flex-row">
+        <Todo />
+        <Todo />
+        <Todo />
+        <Todo />
+      </div>
     </main>
   );
 }
