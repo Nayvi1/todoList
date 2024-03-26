90fdde7032dc95f3d57473a75e507ebea454c26f
Hadi <frisk17h@gmail.com>
2024-03-26 22:51:37 +0430

    Small Css Change

--- src/features/todos/Todo.jsx
+++ src/features/todos/Todo.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 function Todo({ todoText = "Todo" }) {
   return (
-    <div className="w-full hover:bg-lynch-100 p-3 rounded-lg cursor-pointer ">
+    <div className="w-full hover:bg-lynch-100 p-3 rounded-lg cursor-pointer transition">
       <label className="relative group pl-8 cursor-pointer">
         <input type="checkbox" className="absolute w-0 h-0 opacity-0 peer" />
         <span className="absolute top-0 left-0 h-5 w-5 border border-lynch-900 transition-[border] duration-100 rounded-full group-hover:sm:border-[4px] peer-checked:!border-[10px]"></span>
